fd63a19b15670856f40646249ef1ad6e51086959
J <joyaa001@osloskolen.no>
2024-10-24 09:35:22 +0200

    Oppdater main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 for (let indeks = 0; indeks <= 2; indeks++) {
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.InBackground)
     basic.showNumber(3 - indeks)
 }
-music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
 basic.showString("GO!")
+basic.showIcon(IconNames.Happy)
